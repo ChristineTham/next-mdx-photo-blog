b816b676eab04ad237b510a6a20827a09c044fd6
Christine Tham <chris.tham@hellotham.com>
2022-01-18 17:51:08 +1100

    Converted img to next/image

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import cn from 'classnames'
 import Link from 'next/link'
 
@@ -9,16 +10,18 @@ type Props = {
 
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
-    <img
+    <Image
       src={src}
       alt={`Cover Image for ${title}`}
+      sizes="100%"
+      layout="fill"
       className={cn('shadow-sm', {
         'hover:shadow-lg transition-shadow duration-200': slug,
       })}
     />
   )
   return (
-    <div className="sm:mx-0">
+    <div className="sm:mx-0 relative">
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a aria-label={title}>{image}</a>
